9bb129e4ca380d552a3bc75bd62251cc7c3355d8
TheStachelfisch <benbeschow@hotmail.com>
2020-11-12 14:08:42 +0100

    Automatic Code Cleanup

--- PhysicalCache/PhysicalCache.cs
+++ PhysicalCache/PhysicalCache.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Microsoft.VisualBasic;
 using PhysicalCache.Items;
 using PhysicalCache.Utils;
 
@@ -14,28 +12,28 @@ namespace PhysicalCache
         private static bool _IsFolderHidden;
 
         internal List<CacheItem> _cacheItems;
-        
+
         public DirectoryInfo CacheDirectoryInfo { get; private set; }
 
         public PhysicalCache(bool isFolderHidden = true, bool clearOnStartUp = true)
         {
             _IsFolderHidden = isFolderHidden;
             _ClearOnStartUp = clearOnStartUp;
-            
+
             SetupCache();
         }
-        
+
         private void SetupCache()
         {
-            // Can't use this.DirectoryInfo yet, since it's not initialized yet
+            // Can't use this.CacheDirectoryInfo yet, since it's not initialized yet
             if (!Directory.Exists(Constants.CacheFolderName))
                 FolderUtils.CreateHiddenFolder(Constants.CacheFolderName);
-            else if(Directory.Exists(Constants.CacheFolderName) && _ClearOnStartUp)
+            else if (Directory.Exists(Constants.CacheFolderName) && _ClearOnStartUp)
                 FolderUtils.EmptyFolder(new DirectoryInfo(Constants.CacheFolderName));
-            
+
             CacheDirectoryInfo = new DirectoryInfo(Constants.CacheFolderName);
         }
-        
+
         public void Add(CacheItem item)
         {
             if (Contains(item.Key)) return;
@@ -55,12 +53,8 @@ namespace PhysicalCache
             if (key == null) throw new ArgumentException(nameof(key));
 
             foreach (var file in CacheDirectoryInfo.GetFiles())
-            {
                 if (file.Name == key)
-                {
                     return new CacheItem(key, file);
-                }
-            }
 
             return null;
         }
@@ -68,19 +62,18 @@ namespace PhysicalCache
         public bool Contains(string key)
         {
             if (key == null) throw new ArgumentException(nameof(key));
-            
+
             foreach (var file in CacheDirectoryInfo.GetFiles())
-            {
                 if (file.Name == key)
-                {
                     return true;
-                }
-            }
 
             return false;
         }
 
-        public bool Contains(CacheItem item) => Contains(item.Key);
+        public bool Contains(CacheItem item)
+        {
+            return Contains(item.Key);
+        }
 
         public void Dispose()
         {
